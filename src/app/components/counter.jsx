214cb96186755f14a28e3da5472a34b7e40ad6ef
ee7klt <kltah1@gmail.com>
2016-01-28 11:51:52 +0800

    6. add redux. store methods

--- src/app/components/counter.jsx
+++ src/app/components/counter.jsx
@@ -1,5 +1,7 @@
 const React = require('react');
 import expect, { createSpy, spyOn, isSpy } from 'expect';
+import {createStore} from 'redux';
+//const store = createStore(counter);
 
 
 export default class Counter extends React.Component {
@@ -32,10 +34,13 @@ export default class Counter extends React.Component {
 
 
   render () {
+    console.log("this keyword in render is ")
+    console.log(this)
     this.testCounter(0,1,'INCREMENT');
     this.testCounter(2,1,'DECREMENT');
     this.testCounter(1,1,'UNKNOWN');
     this.testCounter(undefined,0,);
+    const store = createStore(this.counter);
     return <div>
       <h1>Counter</h1>
       <button onClick = {() => this.a()}>alert hello</button>

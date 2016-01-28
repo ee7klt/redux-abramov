87e2c04c9c330108804341606c4d37f083b2616a
ee7klt <kltah1@gmail.com>
2016-01-28 11:40:39 +0800

    5. refactor to use switch and ES6 default argument

--- src/app/components/counter.jsx
+++ src/app/components/counter.jsx
@@ -9,15 +9,15 @@ export default class Counter extends React.Component {
     alert('hello')
   }
 
-  counter = (state,action) => {
-    if (typeof state === 'undefined') {
-      return 0;
+  counter = (state = 0,action) => {
+    switch (action.type) {
+      case 'INCREMENT':
+      return state + 1;
+      case 'DECREMENT':
+      return state - 1;
+      default:
+      return state;
     }
-    if (action.type === 'INCREMENT') {
-      return state+1;
-    } else if (action.type === 'DECREMENT') {
-      return state-1;
-    } else return state;
   }
 
   testCounter = (current,next,type) => {

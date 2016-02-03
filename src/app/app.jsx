666737167d3fee93b7239cf5b746ed200fb12663
ee7klt <kltah1@gmail.com>
2016-02-03 11:52:42 +0800

    9. deepfreeze. use concat to return new array

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,18 +1,22 @@
 
 import expect, { createSpy, spyOn, isSpy } from 'expect';
+import deepFreeze from 'deep-freeze';
 
 const addCounter = (list) => {
-  list.push(0);
-  return list;
+  return list.concat([0]);
 };
 
 const testAddCounter =() => {
   const listBefore = [];
   const listAfter = [0];
+
+  deepFreeze(listBefore);
+
   expect(
     addCounter(listBefore)
   ).toEqual(listAfter);
 };
+
 
 testAddCounter();
 console.log('All tests passed.')

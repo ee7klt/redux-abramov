e98b468c00427ffca3b2c46dcbfb4ddd51a93328
ee7klt <kltah1@gmail.com>
2016-02-05 11:30:54 +0800

    toggleTodo mutating

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,82 +1,36 @@
-
 import expect, { createSpy, spyOn, isSpy } from 'expect';
 import deepFreeze from 'deep-freeze';
 
-const addCounter = (list) => {
-  return [...list, 0];
-};
 
-const removeCounter = (list, index) => {
+console.log('toggle todo');
 
-// spread operator more concise
-return [
-  ...list.slice(0, index),
-  ...list.slice(index+1),
-]
+const toggleTodo = (todo) => {
+  // mutating
+  // todo.completed = true;
+  // todo.completed = !todo.completed;  // better!
+  // return todo;
 
-  // return list
-  // .slice(0,index)
-  // .concat(list.slice(index+1));
-};
 
-const incrementCounter = (list,index) => {
-
-  return [
-    ...list.slice(0, index),
-    list[index]+1,
-    ...list.slice(index+1),
-  ]
-
-// list[index]++    // this is mutating
-
-  // return list
-  // .slice(0,index)
-  // .concat([list[index] + 1])
-  // .concat(list.slice(index+1))
 }
 
-const testAddCounter =() => {
-  const listBefore = [];
-  const listAfter = [0];
-
-  deepFreeze(listBefore);
-
+const testToggleTodo = () => {
+  const todoBefore = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: false,
+  };
+  const todoAfter = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: true,
+  };
 
   expect(
-    addCounter(listBefore)
-  ).toEqual(listAfter);
-  console.log('testAddCounter passed.')
+    toggleTodo(todoBefore)
+  ).toEqual(todoAfter)
+
+  console.log('testToggleTodo passed')
 
 };
 
-
-const testRemoveCounter = () => {
-  const listBefore =[0, 10, 20];
-  const listAfter = [0, 20];
-
-  deepFreeze(listBefore);
-
-  expect(
-    removeCounter(listBefore, 1)
-  ).toEqual(listAfter);
-  console.log('testRemoveCounter passed.')
-}
-
-
-const testIncrementCounter = () => {
-  const listBefore =[0, 10, 20];
-  const listAfter = [0, 11,20];
-
-  deepFreeze(listBefore);
-
-  expect(
-    incrementCounter(listBefore, 1)
-  ).toEqual(listAfter);
-
-  console.log('testIncrementCounter passed.');
-}
-
-testAddCounter();
-testRemoveCounter();
-testIncrementCounter();
-console.log('All tests passed.');
+testToggleTodo();

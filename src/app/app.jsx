b8f92428098d56e97a7e252c19a4bf322e3fb4e7
ee7klt <kltah1@gmail.com>
2016-02-06 12:04:51 +0800

    10. Object.assign with empty object target to avoid mutation. last source wins if there is property name conflict

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -6,10 +6,22 @@ console.log('toggle todo');
 
 const toggleTodo = (todo) => {
   // mutating
-  // todo.completed = true;
+  //todo.completed = true;
   // todo.completed = !todo.completed;  // better!
   // return todo;
 
+  // non-mutating option 1:  create new object
+  // but might forget to include some fields.
+  // return {
+  //   id: todo.id,
+  //   text: todo.text,
+  //   completed: !todo.completed,
+  // }
+
+  // non-mutating option 2: ES6 object.assign()
+  return Object.assign({}, todo, {
+    completed: !todo.completed,
+  });
 
 }
 
@@ -24,6 +36,8 @@ const testToggleTodo = () => {
     text: 'Learn Redux',
     completed: true,
   };
+
+  deepFreeze(todoBefore);
 
   expect(
     toggleTodo(todoBefore)

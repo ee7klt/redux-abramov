cb37e329235ea89480dbb2594a54ba4a247f0076
ee7klt <kltah1@gmail.com>
2016-02-07 11:24:49 +0800

    11. add todo reducer. action is plain js object. with type property

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -2,49 +2,49 @@ import expect, { createSpy, spyOn, isSpy } from 'expect';
 import deepFreeze from 'deep-freeze';
 
 
-console.log('toggle todo');
+// reducer: pure function that takes previous state of application
+// and action being dispatched
+// and returns the next state of the app
+// todos: reducer
+// state: previous state (initialized to [] if undefined)
+// action: action being dispatched. plain JS object. must have 'type' property
+const todos = (state=[], action) =>{
 
-const toggleTodo = (todo) => {
-  // mutating
-  //todo.completed = true;
-  // todo.completed = !todo.completed;  // better!
-  // return todo;
+  switch (action.type) {
+    case 'ADD_TODO':
+    return [
+      ...state,
+      {id: action.id, text: action.text, completed: false},
+    ];
+    default:
+    return state
+  }
+}
 
-  // non-mutating option 1:  create new object
-  // but might forget to include some fields.
-  // return {
-  //   id: todo.id,
-  //   text: todo.text,
-  //   completed: !todo.completed,
-  // }
 
-  // non-mutating option 2: ES6 object.assign()
-  return Object.assign({}, todo, {
-    completed: !todo.completed,
-  });
+
+const testAddTodo = () => {
+  const stateBefore = [];
+
+  // action is an object. with a defined type property.
+  const action = {
+    type: 'ADD_TODO',
+    id: 0,
+    text: 'Learn Redux',
+  };
+
+  console.log(action.type)
+  const stateAfter = [{id: 0, text:'Learn Redux', completed: false}];
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(
+    todos(stateBefore, action)
+  ).toEqual(stateAfter);
+  console.log("Test passed: ADD_TODO")
 
 }
 
-const testToggleTodo = () => {
-  const todoBefore = {
-    id: 0,
-    text: 'Learn Redux',
-    completed: false,
-  };
-  const todoAfter = {
-    id: 0,
-    text: 'Learn Redux',
-    completed: true,
-  };
 
-  deepFreeze(todoBefore);
-
-  expect(
-    toggleTodo(todoBefore)
-  ).toEqual(todoAfter)
-
-  console.log('testToggleTodo passed')
-
-};
-
-testToggleTodo();
+testAddTodo();

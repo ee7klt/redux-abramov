36cd7226e6c870e9395420fa63533643dd356fdb
ee7klt <kltah1@gmail.com>
2016-02-16 11:11:52 +0800

    14 reducer composition with objects. combining several reducers in to one

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,5 +1,6 @@
 import expect, { createSpy, spyOn, isSpy } from 'expect';
 import deepFreeze from 'deep-freeze';
+import {createStore} from 'redux';
 
 
 
@@ -19,6 +20,8 @@ const todo = (state = {} ,action) => {
       ...state, completed: !state.completed,
     };
 
+    default:
+    return state;
 
   }
 
@@ -37,7 +40,7 @@ const todos = (state=[], action) =>{
     console.log('ADD_TODO switch selected')
     return [
       ...state,
-      todo({},action),
+      todo(undefined,action),
     ];
 
     case 'TOGGLE_TODO':
@@ -51,7 +54,44 @@ const todos = (state=[], action) =>{
   }
 }
 
+const store = createStore(todos);
 
+console.log('Initial state:');
+console.log(store.getState());
+console.log('----------');
+
+console.log('Dispatching ADD_TODO:');
+store.dispatch({
+  type: 'ADD_TODO',
+  id: 0,
+  text: 'Learn Redux',
+});
+
+console.log('Current state');
+console.log(store.getState());
+console.log('----------');
+
+console.log('Dispatching ADD_TODO:');
+store.dispatch({
+  type: 'ADD_TODO',
+  id: 1,
+  text: 'Go shopping',
+});
+
+console.log('Current state');
+console.log(store.getState());
+console.log('----------');
+
+
+console.log('Dispatching TOGGLE_TODO:');
+store.dispatch({
+  type: 'TOGGLE_TODO',
+  id:0,
+});
+
+console.log('Current state');
+console.log(store.getState());
+console.log('----------');
 
 const testAddTodo = () => {
   const stateBefore = [];

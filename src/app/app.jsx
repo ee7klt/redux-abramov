29ac47a074345ab94d580d3727ee282237602ef6
ee7klt <kltah1@gmail.com>
2016-03-28 10:30:18 +0800

    Remove attributes from <Footer />

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,67 +1,67 @@
-import expect, { createSpy, spyOn, isSpy } from 'expect';
-import deepFreeze from 'deep-freeze';
-import {createStore} from 'redux';
-//import {combineReducers} from 'redux';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Component} from 'react';
+  import expect, { createSpy, spyOn, isSpy } from 'expect';
+  import deepFreeze from 'deep-freeze';
+  import {createStore} from 'redux';
+  //import {combineReducers} from 'redux';
+  import React from 'react';
+  import ReactDOM from 'react-dom';
+  import {Component} from 'react';
 
-const todo = (state = {} ,action) => {
-  switch (action.type) {
+  const todo = (state = {} ,action) => {
+    switch (action.type) {
 
-    case 'ADD_TODO':
-    return {
-      id: action.id, text: action.text, completed: false,
-    };
+      case 'ADD_TODO':
+      return {
+        id: action.id, text: action.text, completed: false,
+      };
 
-    case 'TOGGLE_TODO':
-    if (state.id !== action.id) {
+      case 'TOGGLE_TODO':
+      if (state.id !== action.id) {
+        return state;
+      }
+      return {
+        ...state, completed: !state.completed,
+      };
+
+      default:
+      return state;
+
+    }
+
+  }
+
+  // reducer: pure function that takes previous state of application
+  // and action being dispatched
+  // and returns the next state of the app
+  // todos: reducer
+  // state: previous state (initialized to [] if undefined)
+  // action: action being dispatched. plain JS object. must have 'type' property
+  const todos = (state=[], action) =>{
+
+    switch (action.type) {
+      case 'ADD_TODO':
+      console.log('ADD_TODO switch selected')
+      return [
+        ...state,
+        todo(undefined,action),
+      ];
+
+      case 'TOGGLE_TODO':
+      console.log('TOGGLE_TODO switch selected ',action.id)
+
+      return state.map( t => todo(t, action))
+
+      default:
+      console.log('TODOS: default switch selected')
       return state;
     }
-    return {
-      ...state, completed: !state.completed,
-    };
-
-    default:
-    return state;
-
   }
 
-}
 
-// reducer: pure function that takes previous state of application
-// and action being dispatched
-// and returns the next state of the app
-// todos: reducer
-// state: previous state (initialized to [] if undefined)
-// action: action being dispatched. plain JS object. must have 'type' property
-const todos = (state=[], action) =>{
-
-  switch (action.type) {
-    case 'ADD_TODO':
-    console.log('ADD_TODO switch selected')
-    return [
-      ...state,
-      todo(undefined,action),
-    ];
-
-    case 'TOGGLE_TODO':
-    console.log('TOGGLE_TODO switch selected ',action.id)
-
-    return state.map( t => todo(t, action))
-
-    default:
-    console.log('TODOS: default switch selected')
-    return state;
-  }
-}
-
-
-const AddTodo = ({
-  onAddClick,
-}) => {
-  let input;
-  return <div>
+  const AddTodo = ({
+    onAddClick,
+  }) => {
+    let input;
+    return <div>
     <input ref = {node => {
         input = node;
       }} />
@@ -71,67 +71,67 @@ const AddTodo = ({
           input.value='';
         }}>Add Todo</button>
 
-      </div>
-    }
-
-    const visibilityFilter = (
-      state = 'SHOW_ALL',
-      action
-    ) => {
-      switch (action.type) {
-        case 'SET_VISIBILITY_FILTER':
-        console.log('filter is now ', action.filter)
-        return action.filter;
-        default:
-        return state;
+        </div>
       }
-    }
 
-
-
-    // a single filter link
-    // omit value name 'filter' because of es6 object literal shorthand
-    // filter: SHOW_ALL, SHOW_COMPLETED, SHOW_ACTIVE
-    // children: All, Completed, Active
-    // filter, children are destructured props (see 0.14 changelog)
-    const Link = ({
-      active,
-      children,
-      onClick,
-    }) => {
-
-      if (active) {
-        return (
-          <span>
-            {children}
-          </span>
-        )
-      }
-      else return (
-        <a href='#' onClick = { e => {
-            e.preventDefault();
-            onClick()
+        const visibilityFilter = (
+          state = 'SHOW_ALL',
+          action
+        ) => {
+          switch (action.type) {
+            case 'SET_VISIBILITY_FILTER':
+            console.log('filter is now ', action.filter)
+            return action.filter;
+            default:
+            return state;
           }
         }
 
-        >{children}</a>
-    )
-  }
 
 
-  class FilterLink extends Component {
+      // a single filter link
+      // omit value name 'filter' because of es6 object literal shorthand
+      // filter: SHOW_ALL, SHOW_COMPLETED, SHOW_ACTIVE
+      // children: All, Completed, Active
+      // filter, children are destructured props (see 0.14 changelog)
+      const Link = ({
+        active,
+        children,
+        onClick,
+      }) => {
 
-
-
-        componentDidMount() {
-          this.unsubscribe = store.subscribe(() =>
-            this.forceUpdate()
-          );
+        if (active) {
+          return (
+            <span>
+            {children}
+            </span>
+          )
         }
+        else return (
+          <a href='#' onClick = { e => {
+              e.preventDefault();
+              onClick()
+            }
+          }
 
-        componentWillUnmount() {
-          this.unsubscribe();
-        }
+          >{children}</a>
+      )
+    }
+
+
+    class FilterLink extends Component {
+
+
+
+            componentDidMount() {
+              this.unsubscribe = store.subscribe(() =>
+              this.forceUpdate()
+            );
+          }
+
+          componentWillUnmount() {
+            this.unsubscribe();
+          }
 
 
     render() {
@@ -143,16 +143,16 @@ const AddTodo = ({
       const state = store.getState();
       return (
         <Link
-          active = {filter === state.visibilityFilter}
-          onClick = {() => store.dispatch({
-            type: 'SET_VISIBILITY_FILTER',
-            filter: filter,
-          })}
+        active = {filter === state.visibilityFilter}
+        onClick = {() => store.dispatch({
+          type: 'SET_VISIBILITY_FILTER',
+          filter: filter,
+        })}
 
 
-          >  {children} </Link>
-      )
-    }
+        >  {children}</Link>
+    )
+  }
 
   }
 
@@ -160,14 +160,14 @@ const AddTodo = ({
 
   const Footer = () => {
     return   <p>
-      Show:
-      {'  '}
-      <FilterLink filter = 'SHOW_ALL' children = 'All' />
-      {'  '}
-      <FilterLink filter = 'SHOW_COMPLETED' children = 'Completed'  />
-      {'  '}
-      <FilterLink filter = 'SHOW_ACTIVE' children = 'Active'  />
-    </p>
+    Show:
+    {'  '}
+    <FilterLink filter = 'SHOW_ALL' children = 'All' />
+  {'  '}
+  <FilterLink filter = 'SHOW_COMPLETED' children = 'Completed'  />
+  {'  '}
+  <FilterLink filter = 'SHOW_ACTIVE' children = 'Active'  />
+  </p>
   }
 
   // Todo presentational component
@@ -179,11 +179,11 @@ const AddTodo = ({
   }) => {
     console.log(text);
     return <li onClick = {onClick}
-      style = {{
-        textDecoration:
-        completed ? 'line-through' : 'none',
-      }}>
-      {text}
+    style = {{
+      textDecoration:
+      completed ? 'line-through' : 'none',
+    }}>
+    {text}
     </li>
   };
 
@@ -196,35 +196,35 @@ const AddTodo = ({
   }) => {
     console.log('TodoList presentational component');
     return <ul>
-      {todos.map(todo => {
-        return <Todo
-          key = {todo.id}
-          {...todo}
-          onClick = {() => onTodoClick(todo.id)}
-          />
-      })}
-    </ul>
+    {todos.map(todo => {
+      return <Todo
+      key = {todo.id}
+      {...todo}
+      onClick = {() => onTodoClick(todo.id)}
+      />
+  })}
+  </ul>
 
   };
   // filter out the visibile todos based on visibilityFilter state
 
-  const getVisibleTodos = (todos, filter) => {
-    switch (filter) {
-      case 'SHOW_ALL':
-      console.log('getting ALL todos')
-      return todos
-      case 'SHOW_COMPLETED':
-      console.log('getting COMPLETED todos')
-      return todos.filter(t => {
-        return t.completed
-      })
-      case 'SHOW_ACTIVE':
-      console.log('getting ACTIVE todos')
-      return todos.filter(t => {return !t.completed})
-      default:
-      return todos;
+    const getVisibleTodos = (todos, filter) => {
+      switch (filter) {
+        case 'SHOW_ALL':
+        console.log('getting ALL todos')
+        return todos
+        case 'SHOW_COMPLETED':
+        console.log('getting COMPLETED todos')
+        return todos.filter(t => {
+          return t.completed
+        })
+        case 'SHOW_ACTIVE':
+        console.log('getting ACTIVE todos')
+        return todos.filter(t => {return !t.completed})
+        default:
+        return todos;
+      }
     }
-  }
 
   const combineReducers = (reducers) => {
     return (state = {}, action) => {
@@ -253,103 +253,95 @@ const AddTodo = ({
     visibilityFilter,
   }) => {
     return <div>
-      <AddTodo onAddClick = {
-          input => {
-            store.dispatch({
-              type:'ADD_TODO',
-              text: input,
-              id: nextTodoId++,
-            })
+    <AddTodo onAddClick = {
+        input => {
+          store.dispatch({
+            type:'ADD_TODO',
+            text: input,
+            id: nextTodoId++,
+          })
+        }
+      }/>
+
+      <TodoList
+      todos = {getVisibleTodos(
+        todos,
+        visibilityFilter
+      )}
+      onTodoClick = {(id) => {
+        store.dispatch(
+          {
+            type: 'TOGGLE_TODO',
+            id,
           }
-        }/>
+        )
+      }}
+      />
 
-        <TodoList
-          todos = {getVisibleTodos(
-            todos,
-            visibilityFilter
-          )}
-          onTodoClick = {(id) => {
-            store.dispatch(
-              {
-                type: 'TOGGLE_TODO',
-                id,
-              }
-            )
-          }}
-          />
+    <Footer />
+  </div>
+}
 
-        <Footer
-          visibilityFilter = {visibilityFilter}
-          onClick = {(filter) => {
-            store.dispatch({
-              type:'SET_VISIBILITY_FILTER',
-              filter,
-            })
-          }}
-          />
-      </div>
-    }
+  const store = createStore(todoApp);
 
-    const store = createStore(todoApp);
+  const render = () => {
+    //console.log("render triggered")
+    ReactDOM.render(
+      <TodoApp
+      {...store.getState()}
+      />,
+    document.getElementById('root')
+  );
+  };
 
-    const render = () => {
-      //console.log("render triggered")
-      ReactDOM.render(
-        <TodoApp
-          {...store.getState()}
-          />,
-        document.getElementById('root')
-      );
-    };
-
-    store.subscribe(render);
-    render();
+  store.subscribe(render);
+  render();
 
 
-    const testGetVisibleTodos = () => {
-      const todos =  [
-        {id: 0, text:'Learn Redux', completed: false},
-        {id: 0, text:'Learn Relay', completed: true},
-      ]
+  const testGetVisibleTodos = () => {
+    const todos =  [
+      {id: 0, text:'Learn Redux', completed: false},
+      {id: 0, text:'Learn Relay', completed: true},
+    ]
 
-      const  filtered = [{id: 0, text:'Learn Relay', completed: true}]
+    const  filtered = [{id: 0, text:'Learn Relay', completed: true}]
 
-      deepFreeze(todos);
-      expect(getVisibleTodos(todos, 'SHOW_COMPLETED'))
-      .toEqual(filtered);
-      console.log('getVisibleTodos passed')
+    deepFreeze(todos);
+    expect(getVisibleTodos(todos, 'SHOW_COMPLETED'))
+    .toEqual(filtered);
+    console.log('getVisibleTodos passed')
 
-    }
-    testGetVisibleTodos();
+  }
+  testGetVisibleTodos();
 
 
-    const testTodoApp = () => {
-      const stateBefore = {
-        todos: [{id: 0, text:'Learn Redux', completed: false}],
-        visibilityFilter: 'SHOW_ALL',
-      };
-
-      // action is an object. with a defined type property.
-      const action = {
-        type: 'ADD_TODO',
-        id: 1,
-        text: 'Go shopping',
-      };
-
-      const stateAfter = {
-        todos: [{id: 0, text:'Learn Redux', completed: false},
-        {id: 1, text:'Go shopping', completed: false},
-      ],
+  const testTodoApp = () => {
+    const stateBefore = {
+      todos: [{id: 0, text:'Learn Redux', completed: false}],
       visibilityFilter: 'SHOW_ALL',
     };
 
-    deepFreeze(stateBefore);
-    deepFreeze(action);
+    // action is an object. with a defined type property.
+    const action = {
+      type: 'ADD_TODO',
+      id: 1,
+      text: 'Go shopping',
+    };
 
-    expect(
-      todoApp(stateBefore, action)
-    ).toEqual(stateAfter);
-    console.log("Test passed: todoApp")
+    const stateAfter = {
+      todos: [{id: 0, text:'Learn Redux', completed: false},
+      {id: 1, text:'Go shopping', completed: false},
+    ],
+    visibilityFilter: 'SHOW_ALL',
+  };
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(
+    todoApp(stateBefore, action)
+  ).toEqual(stateAfter);
+  console.log("Test passed: todoApp")
 
   }
 
